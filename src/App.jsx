77f96c5399f13e408883f0a99853d57567cd5002
VianGalop <matcaptu10@gmail.com>
2024-04-17 16:03:41 -0600

    iniciando Portafolio

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='contenedor'>
+        <nav className='navegator'>
+          <ul className='nav nav-tabs'>
+            <li className='link'></li>
+            <li className='link'></li>
+            <li className='link'></li>
+          </ul>
+        </nav>
+
+        <div className='portadas'>
+
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
